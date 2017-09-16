42ea43c0f7427cc4a51908eb4302fdc4bdd19cba
Hien Phan The <hien.phanthe@pycogroup.com>
2017-09-16 17:49:28 +0700

    update folder name

---
+++ 100Common/100Common/MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using Plugin.Connectivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace OneHundredCommonThings
+{
+    public partial class MainPage : ContentPage
+    {
+        private ItemViewModel itemsService;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.itemsService = new ItemViewModel();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            this.BusyIndicator.IsVisible = true;
+            this.BusyIndicator.IsRunning = true;
+            try
+            {
+                await this.itemsService.PopulateDataAsync(false);
+                this.BindingContext = this.itemsService.Items;
+            }
+            catch(InvalidOperationException)
+            {
+                await DisplayAlert("Error", "Check your network connection.", "OK");
+                return;
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+                return;
+            }
+            finally
+            {
+                this.BusyIndicator.IsVisible = false;
+                this.BusyIndicator.IsRunning = false;
+            }
+        }
+
+        private async Task LoadDataAsync()
+        {
+            this.BusyIndicator.IsVisible = true;
+            this.BusyIndicator.IsRunning = true;
+            try
+            {
+                await this.itemsService.PopulateDataAsync(true);
+                this.BindingContext = this.itemsService.Items;
+            }
+            catch (InvalidOperationException)
+            {
+                await DisplayAlert("Error", "Check your network connection.", "OK");
+                return;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+                return;
+            }
+            finally
+            {
+                this.BusyIndicator.IsVisible = false;
+                this.BusyIndicator.IsRunning = false;
+            }
+        }
+
+        private async void RssView_Refreshing(object sender, EventArgs e)
+        {
+            await LoadDataAsync();
+        }
+
+        private async void RefreshButton_Clicked(object sender, EventArgs e)
+        {
+            await LoadDataAsync();
+        }
+
+        private async void RssView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var selected = e.Item as Item;
+
+            if (selected != null)
+            {
+                await Navigation.PushAsync(new WebContentPage(selected.Link));
+            }
+        }
+    }
+}
---
+++ 100Common/100Common/ViewModel/BaseServiceVM.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using OneHundredCommonThings.Model;
+using SQLite;
+using Xamarin.Forms;
+
+namespace OneHundredCommonThings.ViewModel
+{
+    public abstract class BaseServiceVM<T>: INotifyPropertyChanged where T: BaseModel, new()
+    {
+		private bool isBusy;
+		public bool IsBusy
+		{
+			get
+			{
+				return isBusy;
+			}
+			set
+			{
+				isBusy = value;
+				OnPropertyChanged();
+			}
+		}
+
+        private DataService<T> dataService;
+
+        public BaseServiceVM()
+		{
+            dataService = new DataService<T>();
+		}
+
+        public ObservableCollection<T> ModelCollection { get; set; }
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void OnPropertyChanged([CallerMemberName]string name = "") =>
+					PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+		public async Task PopulateDataAsync(bool refresh)
+		{
+			if (IsBusy)
+			{
+				return;
+			}
+
+            IEnumerable<T> onlineData = null;
+			if (refresh == true && App.IsConnected)
+			{
+				try
+				{
+					IsBusy = true;
+                    onlineData = await dataService.PopulateDataOnlineAsync();
+                    this.ModelCollection = new ObservableCollection<T>(onlineData);
+                    return;
+				}
+                catch(Exception)
+				{
+                    return;
+				}
+				finally
+				{
+					IsBusy = false;
+				}
+			}
+
+            onlineData = await dataService.PopulateDataOfflineAsync();
+			this.ModelCollection = new ObservableCollection<T>(onlineData);
+		}
+    }
+}
