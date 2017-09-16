e84180b94fb39a5168f8fe4b64ae60e913562bcb
Hien Phan The <hien.phanthe@pycogroup.com>
2017-09-16 23:43:29 +0700

    add topic view model

--- 100Common/100Common/View/TopicsView.xaml.cs
+++ 100Common/100Common/View/TopicsView.xaml.cs
@@ -1,20 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
+using OneHundredCommonThings.ViewModel;
 using OneHundredCommonThings.Model;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace OneHundredCommonThings.View
 {
     public partial class TopicsView : ContentView
     {
-        public TopicsView()
+        private TopicViewModel topicVM;
+
+		public string TopicUrl
+		{
+			get { return base.GetValue(TopicUrlProperty).ToString(); }
+			set { base.SetValue(TopicUrlProperty, value); }
+		} 
+
+		public static readonly BindableProperty TopicUrlProperty = BindableProperty.Create(
+														 propertyName: "TopicUrl",
+														 returnType: typeof(string),
+														 declaringType: typeof(TopicsView),
+														 defaultValue: "",
+														 defaultBindingMode: BindingMode.TwoWay,
+														 propertyChanged: topicUrlPropertyChanged);
+        
+		public TopicsView()
         {
             InitializeComponent();
-            var topics = new [] { 
-                new Topic() { TopicName = "Hien test"}
-            };
-            this.BindingContext = new ObservableCollection<Topic>(topics);
         }
+
+
+		public async Task LoadControl()
+		{
+            this.topicVM = new TopicViewModel(TopicUrl, "Topic");
+			await this.topicVM.PopulateDataAsync(true);
+
+			var scrollableContent = new StackLayout()
+			{
+				Orientation = StackOrientation.Horizontal,
+				HorizontalOptions = LayoutOptions.Fill
+			};
+
+			foreach (var topic in this.topicVM.ModelCollection)
+			{
+				var label = new Label() { HeightRequest = 30, BackgroundColor = Color.Green };
+                label.Text = topic.Description;
+				scrollableContent.Children.Add(label);
+			}
+
+			this.Content = new ScrollView()
+			{
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				Orientation = ScrollOrientation.Horizontal,
+				Content = scrollableContent,
+			};
+		}
+
+		private static void topicUrlPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+		{
+			var control = (TopicsView)bindable;
+            control.TopicUrl = newValue.ToString();
+			control.LoadControl();
+		}
     }
 }
---
+++ 100Common/100Common/ViewModel/TopicViewModel.cs
@@ -0,0 +1,13 @@
+﻿using OneHundredCommonThings.Model;
+using OneHundredCommonThings.Service;
+
+namespace OneHundredCommonThings.ViewModel
+{
+    public class TopicViewModel : BaseServiceVM<Topic>
+	{
+        public TopicViewModel(string url = null, string propertyName = null)
+            : base(new TopicDataService(url, propertyName)) {
+            
+        }
+	}
+}
